54e08d3832cc63e496ac3dd8a41d8d6f81b890b2
maayan3330 <maayan3330@gmail.com>
2025-05-13 13:07:44 +0300

    commit

--- ex1.py
+++ ex1.py
@@ -168,10 +168,6 @@ class PressurePlateProblem(search.Problem):
         # ##############################################################
         if self.dead_end_due_to_stuck_blocks(state, direction, map_for_state):
             return results
-        # ###############################################################
-        if self.hallway_to_dead_end(state, direction, map_for_state):
-            return results
-       
 
         # check now for good cases to insert to the states :
         row_of_agent , col_of_agent = state[0]
@@ -191,7 +187,7 @@ class PressurePlateProblem(search.Problem):
             # keep the all info about the "key blockes"
             new_state = (new_agent_placement, tuple(sorted(key_blocks)), frozenset(open_doors), frozenset(plates_covered.items()))
            
-            if new_state not in self.visited_states:
+            if (direction,new_state) not in self.visited_states:
                 self.visited_states.add((direction,new_state))
                 results.append((direction, new_state))
 
@@ -209,7 +205,7 @@ class PressurePlateProblem(search.Problem):
                     # update all
                     new_state = ((one_move_row, one_move_col), tuple(sorted(key_blocks)), frozenset(open_doors), frozenset(plates_covered.items()))
                    
-                    if new_state not in self.visited_states:
+                    if (direction,new_state) not in self.visited_states:
                         self.visited_states.add((direction,new_state))
                         results.append((direction, new_state))
                     return results
@@ -234,7 +230,7 @@ class PressurePlateProblem(search.Problem):
                     new_agent_placement = (one_move_row, one_move_col)
                     new_state = (new_agent_placement, tuple(sorted(key_blocks)), frozenset(open_doors),frozenset(plates_covered.items()))
                  
-                    if new_state not in self.visited_states:
+                    if (direction,new_state) not in self.visited_states:
                         self.visited_states.add((direction, new_state))
                         results.append((direction, new_state))
                     return results
@@ -356,51 +352,7 @@ class PressurePlateProblem(search.Problem):
 
         return False  # Not stuck
 
-    # case 7
-    def hallway_to_dead_end(self, state, direction, new_map):
-        row_of_agent , col_of_agent = state[0]
-        direction_row, direction_col = DIRECTIONS[direction]
-
-        one_move_row, one_move_col = row_of_agent + direction_row, col_of_agent + direction_col
-        two_move_row, two_move_col = row_of_agent + 2 * direction_row, col_of_agent + 2 * direction_col
-        three_move_row, three_move_col = row_of_agent + 3 * direction_row, col_of_agent + 3 * direction_col
-
-        # first check if the move push a cube - so if in the other plate there is a key!
-        if new_map[one_move_row][one_move_col] in KEY_BLOCKS:
-            # now there is a cube - first check if the placement after it is in the bounderis
-            if not (0 <= two_move_row < self.rows and 0 <= two_move_col < self.cols):
-                # the next step is out of bounderies for ROW and COL
-                return True
-            if not (0 <= three_move_row < self.rows and 0 <= three_move_col < self.cols):
-                # the next step is out of bounderies for ROW and COL
-                return True
-            # the next step is on floor
-            if (new_map[two_move_row][two_move_col] == FLOOR) and (new_map[three_move_row][three_move_col] == FLOOR) :
-                # and it will stack
-                if self.is_block_stuck(three_move_row, three_move_col, new_map) and self.is_block_stuck_2_PLUS(two_move_row, two_move_col, new_map):
-                    # it will push a cune to a problem placmnebt - נבדוק שבאמת צריך את הקוביה
-        
-                    return True
-        
-        return False
-
-    def is_block_stuck_2_PLUS(self, r, c, new_map):
-        # אם הקובייה על לחצן – היא לא תקועה
-        if (r, c) in [(i, j) for i, j, _ in self.plates_info]:
-            return False
-
-        walls = 0
-        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            nr, nc = r + dr, c + dc
-
-            # בדיקה שלא חורג מגבולות המפה
-            if 0 <= nr < self.rows and 0 <= nc < self.cols:
-                if new_map[nr][nc] == WALL:
-                    walls += 1
-            else:
-                walls += 1  # מחוץ למפה = קיר
-
-        return walls >= 2  # פינה אם יש לפחות שני קירות
+   
 
     def goal_test(self, state):
         """ given a state, checks if this is the goal state, compares to the created goal state returns True/False"""
